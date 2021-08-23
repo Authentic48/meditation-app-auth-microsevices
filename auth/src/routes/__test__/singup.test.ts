4ecb003beaadd521b7bf474c59752c226f2b9d0b
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-23 17:47:40 +0700

    Signup unit test  done

--- auth/src/routes/__test__/singup.test.ts
+++ auth/src/routes/__test__/singup.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 describe('POST /api/auth/register', () => {
-  it('returns a 201 on successful signup', () => {
+  it('returns a 201 on successful signup', async () => {
     return request(app)
       .post('/api/auth/register')
       .send({
@@ -11,5 +11,38 @@ describe('POST /api/auth/register', () => {
         password: 'password',
       })
       .expect(201);
+  });
+
+  it('returns a 400 with invalid email', async () => {
+    return request(app)
+      .post('/api/auth/register')
+      .send({
+        name: 'test',
+        email: 'testtest.com',
+        password: 'password',
+      })
+      .expect(400);
+  });
+
+  it('returns a 400 with invalid password', async () => {
+    return request(app)
+      .post('/api/auth/register')
+      .send({
+        name: 'test',
+        email: 'test@test.com',
+        password: 'pas',
+      })
+      .expect(400);
+  });
+
+  it('returns a 400 with invalid name', async () => {
+    return request(app)
+      .post('/api/auth/register')
+      .send({
+        name: '',
+        email: 'test@test.com',
+        password: 'password',
+      })
+      .expect(400);
   });
 });
