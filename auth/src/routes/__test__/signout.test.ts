12de8c14b786527c9aab567265d8bc661b3b730f
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-23 19:01:24 +0700

    Current User unit test done

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -17,8 +17,6 @@ describe('POST /api/auth/signout ', () => {
       .send({})
       .expect(200);
 
-    console.log(response.get('Set-Cookie'));
-
     expect(response.get('Set-Cookie')[0]).toEqual(
       'express:sess=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly'
     );
