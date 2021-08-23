b3df372db1b2f1a39c0e01e662f5ef7e4860c2a8
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-23 14:23:39 +0700

    CurrentUser Middleware added

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,19 +1,16 @@
 import express, { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
+import { currentLogInUser } from '../middlewares/currentUser';
+
 const route = express.Router();
 
-route.get('/api/auth/currentuser', async (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.json({ currentUser: null });
+route.get(
+  '/api/auth/currentuser',
+  currentLogInUser,
+  (req: Request, res: Response) => {
+    res.json({ currentUser: req.currentUser });
   }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    return res.json({ currentUser: payload });
-  } catch (error) {
-    return res.json({ currentUser: null });
-  }
-});
+);
 
 export { route as currentUser };
