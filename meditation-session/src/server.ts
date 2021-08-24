f511ff5971ff17dedaed7a4826fd6d91eb5476d3
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-24 13:07:17 +0700

    Meditation service  setup done

---
+++ meditation-session/src/server.ts
@@ -0,0 +1,17 @@
+import { connectDB } from './config/db';
+import { app } from './app';
+
+const PORT = 5000;
+
+const start = () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+  connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`App in ${process.env.NODE_ENV} is running on ${PORT}`);
+  });
+};
+
+start();
